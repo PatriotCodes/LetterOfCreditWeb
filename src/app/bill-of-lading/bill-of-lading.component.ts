6717acd0922238a02571bca77cd8c2fe80f57fbd
CaisR3 <cais.manai@r3.com>
2018-05-17 19:03:08 +0100

    Field placeholders more explicit

--- src/app/bill-of-lading/bill-of-lading.component.ts
+++ src/app/bill-of-lading/bill-of-lading.component.ts
@@ -45,30 +45,30 @@ export class BillOfLadingComponent {
     this.bol.goodsQuantity = 10000;
     this.bol.dateOfShipment = d;
 
-    this.bol.portOfLoadingCountry = 'China'
-    this.bol.portOfLoadingCity = 'Shenzhen'
-    this.bol.portOfLoadingAddress = '123 Street'
+    this.bol.portOfLoadingCountry = 'China';
+    this.bol.portOfLoadingCity = 'Shenzhen';
+    this.bol.portOfLoadingAddress = '123 Street';
 
-    this.bol.portOfDischargeCountry = 'USA'
-    this.bol.portOfDischargeCity = 'Des Moines'
-    this.bol.portOfDischargeAddress = 'ABC Street'
+    this.bol.portOfDischargeCountry = 'USA';
+    this.bol.portOfDischargeCity = 'Des Moines';
+    this.bol.portOfDischargeAddress = 'ABC Street';
 
     this.bol.shipper = this.loc[0].beneficiary;
-    this.bol.notifyName = 'Foo Bar'
-    this.bol.notifyAddress = '123 Street, London'
-    this.bol.notifyPhone = '+442076909856'
+    this.bol.notifyName = 'Foo Bar';
+    this.bol.notifyAddress = '123 Street, London';
+    this.bol.notifyPhone = '+442076909856';
 
-    this.bol.consigneeName = this.loc[0].applicant
+    this.bol.consigneeName = this.loc[0].applicant;
     this.bol.consigneeAddress = '123 Main St. Awesome Town, ZZ 11111';
-    this.bol.consigneePhone = '+0027590043622'
+    this.bol.consigneePhone = '+0027590043622';
 
-    this.bol.grossWeight = 1000
-    this.bol.grossWeightUnit = 'KG'
+    this.bol.grossWeight = 1000;
+    this.bol.grossWeightUnit = 'KG';
 
-    this.bol.placeOfReceiptCountry = 'USA'
-    this.bol.placeOfReceiptCity = 'Des Moines'
+    this.bol.placeOfReceiptCountry = 'USA';
+    this.bol.placeOfReceiptCity = 'Des Moines';
 
-    this.bol.buyer = this.bol.consigneeName
+    this.bol.buyer = this.bol.consigneeName;
     this.bol.advisingBank = this.loc[0].advisingBank;
   }
 
