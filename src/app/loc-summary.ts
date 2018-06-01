1ad8a23fe9b89c9643d7be0ff4b19aa8a87281fb
CaisR3 <cais.manai@r3.com>
2018-06-01 14:06:56 +0100

    Changing to letterOfCreditApplicationID from txRef

--- src/app/loc-summary.ts
+++ src/app/loc-summary.ts
@@ -14,7 +14,7 @@ export class LocSummary implements Serializable<LocSummary> {
   buyerPayment: Boolean;
 
   deserialize(input: any) {
-    this.id = input.first;
+    this.id = input.third.props.letterOfCreditApplicationID;
     this.beneficiary = input.third.props.beneficiary.substring(2, input.third.props.beneficiary.indexOf(","));
     this.applicant = input.third.props.applicant.substring(2, input.third.props.applicant.indexOf(","));
     this.amount = input.third.props.amount;
